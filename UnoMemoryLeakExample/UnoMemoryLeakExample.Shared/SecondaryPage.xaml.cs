26289edc47fa86b3a54bfac305741185822140c9
CONDUCE\ross.conyers <ross.conyers@conduce.net>
2020-08-31 10:50:31 +0100

    Create minimal repro project

---
+++ UnoMemoryLeakExample/UnoMemoryLeakExample.Shared/SecondaryPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using UnoMemoryLeakExample.Shared.ViewModels;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UnoMemoryLeakExample.Shared
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SecondaryPage : Page
+    {
+        private SecondaryPageViewModel VM { get; set; }
+
+        public SecondaryPage()
+        {
+            this.InitializeComponent();
+            this.VM = new SecondaryPageViewModel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.GoBack();
+        }
+    }
+}
---
+++ UnoMemoryLeakExample/UnoMemoryLeakExample.Shared/ViewModels/SecondaryPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnoMemoryLeakExample.Shared.ViewModels
+{
+    class SecondaryPageViewModel
+    {
+        public string TestString { get; set; } = "This is a View Model binding.";
+    }
+}

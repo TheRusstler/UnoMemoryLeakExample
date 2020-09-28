0a67d2c80dc6ae450e15a029d5e804b12e2b1bce
James Russell <james.russell@conduce.net>
2020-09-28 16:25:07 +0000

    Added views memory statistics logging

--- UnoMemoryLeakExample/UnoMemoryLeakExample.Shared/App.xaml.cs
+++ UnoMemoryLeakExample/UnoMemoryLeakExample.Shared/App.xaml.cs
@@ -1,70 +1,27 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace UnoMemoryLeakExample
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     sealed partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             ConfigureFilters(global::Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory);
-
+            EnableViewsMemoryStatistics();
             this.InitializeComponent();
-            this.Suspending += OnSuspending;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-#if DEBUG
-			if (System.Diagnostics.Debugger.IsAttached)
-			{
-				// this.DebugSettings.EnableFrameRateCounter = true;
-			}
-#endif
             Frame rootFrame = Windows.UI.Xaml.Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
             if (rootFrame == null)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
                 Windows.UI.Xaml.Window.Current.Content = rootFrame;
             }
 
@@ -72,45 +29,13 @@ namespace UnoMemoryLeakExample
             {
                 if (rootFrame.Content == null)
                 {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
                     rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
-                // Ensure the current window is active
+
                 Windows.UI.Xaml.Window.Current.Activate();
             }
         }
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception($"Failed to load {e.SourcePageType.FullName}: {e.Exception}");
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
-        }
-
-
-        /// <summary>
-        /// Configures global logging
-        /// </summary>
-        /// <param name="factory"></param>
         static void ConfigureFilters(ILoggerFactory factory)
         {
             factory
@@ -138,7 +63,7 @@ namespace UnoMemoryLeakExample
 						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
 
 						// DependencyObject memory references tracking
-						// { "ReferenceHolder", LogLevel.Debug },
+						 { "ReferenceHolder", LogLevel.Debug },
 
 						// ListView-related messages
 						// { "Windows.UI.Xaml.Controls.ListViewBase", LogLevel.Debug },
@@ -151,12 +76,48 @@ namespace UnoMemoryLeakExample
 						// { "Windows.UI.Xaml.Controls.NativeListViewBaseAdapter", LogLevel.Debug }, //Android
 						// { "Windows.UI.Xaml.Controls.BufferViewCache", LogLevel.Debug }, //Android
 						// { "Windows.UI.Xaml.Controls.VirtualizingPanelGenerator", LogLevel.Debug }, //WASM
-					}
+
+                        { "Uno.UI.DataBinding", LogLevel.Debug },
+                        { "Uno.UI.DataBinding.BinderReferenceHolder", LogLevel.Information }
+                    }
                 )
 #if DEBUG
-				.AddConsole(LogLevel.Debug);
+                .AddConsole(LogLevel.Debug);
 #else
                 .AddConsole(LogLevel.Information);
+#endif
+        }
+
+        private void EnableViewsMemoryStatistics()
+        {
+#if __IOS__
+            var unused = Windows.UI.Xaml.Window.Current.Dispatcher.RunAsync(
+                Windows.UI.Core.CoreDispatcherPriority.Normal,
+                async () =>
+                {
+                    Uno.UI.DataBinding.BinderReferenceHolder.IsEnabled = true;
+
+                    while (true)
+                    {
+                        await System.Threading.Tasks.Task.Delay(1500);
+
+                        try
+                        {
+                            Uno.UI.DataBinding.BinderReferenceHolder.LogReport();
+
+                            var inactiveInstances = Uno.UI.DataBinding.BinderReferenceHolder.GetInactiveViewBinders();
+
+                            // Force the variable to be kept by the linker so we can see it with the debugger.
+                            // Put a breakpoint on this line to dig into the inactive views.
+                            inactiveInstances.ToString();
+                        }
+                        catch (Exception)
+                        {
+                            System.Diagnostics.Debug.WriteLine("Report generation failed");
+                        }
+                    }
+                }
+            );
 #endif
         }
     }
